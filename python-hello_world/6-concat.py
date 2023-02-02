2342174d6c71f1c9ea89987dff57bed9237ca183
malong_joshua <121355936+Malongmak@users.noreply.github.com>
2023-02-02 20:50:47 +0200

    Update 6-concat.py

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,21 +1,5 @@
 #!/usr/bin/python3
-/**
-* main - A program that prints the size of various computer types
-* Return 0 (success)
-*/
-
-int main(void)
-{
-  char a;
-  int b;
-  long int c;
-  long long int d;
-  float f;
-  
-  printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-  printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
-  printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-  printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-  printf("Size of a float: %lu byte(S)\n", (unsigned long)sizeof(f));
-  return (0);
-}
+Str1 = "Holberton"
+Str2 = "School"
+Str = (" " + str2)
+printf(Welcome to {}!".format(str1))
