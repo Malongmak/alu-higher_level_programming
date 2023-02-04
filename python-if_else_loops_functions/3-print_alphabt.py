b7f4968dc89820e43c5d0e4187b912060fee6e7e
Malongmak <j.malong@alustudent.com>
2023-02-04 05:05:21 -0800

    Updated

---
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,7 @@
+#!/bin/python3
+for alpha_letters in range(ord('a'),
+        ord('Z')+1):
+if alpha_letters == 'e' or
+    alpha_letters == 'q':
+        continue
+    print("{:c}".format(alpha_letters), end=" ")
