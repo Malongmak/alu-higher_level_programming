4e13f6f264cd51a8c9296b50dfc4cd5ec3b8f5f7
malong_joshua <121355936+Malongmak@users.noreply.github.com>
2023-02-06 18:14:50 +0200

    Update 6-concat.py

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-Str1 = "Holberton"
+str1 = "Holberton"
 str2 = "School"
-str1 = (" " + str2)
-printf(Welcome to {}!".format(str1))
+str1 = str1+' '+str2
+print(f"Welcome to {str1}!")
