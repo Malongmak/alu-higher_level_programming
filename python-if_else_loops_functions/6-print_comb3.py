a0f2527478ff35eda9cea064877e1e01f2473cba
malong_joshua <121355936+Malongmak@users.noreply.github.com>
2023-02-06 22:02:43 +0200

    Update 6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -4,4 +4,4 @@ for x in range(10):
         if x == 8 and y == 9:
             print("{}{}".format(x, y))
         else:
-            print("{}{}".format(x, y), end=",")
+            print("{}{}".format(x, y), end=", ")
