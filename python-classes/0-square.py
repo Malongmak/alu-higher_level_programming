84c20653f1e5d225a7c86dc13c2736791de37dcc
Malongmak <j.malong@alustudent.com>
2023-03-12 21:15:47 +0200

    0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Class Square that defines a square.
-"""
+    """
 
 
-Class Square:
-    """Class Square that defines asquare.
+class Square:
+    """Class Square that defines a square.
     """
     pass
