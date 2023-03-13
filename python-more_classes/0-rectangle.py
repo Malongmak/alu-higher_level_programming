799e56c566f64a186dc44224f9acf51e526627cd
Malongmak <j.malong@alustudent.com>
2023-03-13 10:36:04 +0200

    0-rectangle.py

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+An empty class Rectangle that defines a rectangle
+"""
+
+
+
+class Rectangle:
+    """Creates a rectangle."""
+    pass
---
+++ python-more_classes/README.md
@@ -0,0 +1,2 @@
+#!/usr/bin/python3
+Python - More Classes and Objects
