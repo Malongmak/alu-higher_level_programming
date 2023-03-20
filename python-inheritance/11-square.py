dbddeed4a032278d062b022443d94311b57e73c4
Malongmak <j.malong@alustudent.com>
2023-03-20 22:13:19 +0200

    11-square.py

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,7 +1,25 @@
 #!/usr/bin/python3
-Square = __import__('11-square').Square
+"""Inherits BaseGeometry class"""
 
-s = Square(13)
 
-print(s)
-print(s.area())
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Inherits from Rectangle."""
+
+    def __init__(self, size):
+        """Initializes data."""
+
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def __str__(self):
+        """Returns [Square] <width>/<height>."""
+        return str("[Square] {}/{}".format(self.__size, self.__size))
+
+    def area(self):
+        """Area of Square"""
+
+        return self.__size ** 2
