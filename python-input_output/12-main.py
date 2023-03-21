2ef09447d4b946bc3fae977073b376f1286fb856
malong_joshua <121355936+Malongmak@users.noreply.github.com>
2023-03-21 23:40:25 +0200

    Update 12-main.py

--- python-input_output/12-main.py
+++ python-input_output/12-main.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
-"""
-12-main
-"""
-pascal_triangle = __import__('12-pascal_triangle').pascal_triangle
-
-def print_triangle(triangle):
-    """
-    Print the triangle
-    """
-    for row in triangle:
-        print("[{}]".format(",".join([str(x) for x in row])))
+"""Returns list of pascal triangles"""
 
 
-if __name__ == "__main__":
-    print_triangle(pascal_triangle(5))
+def pascal_triangle(n):
+    """Returns list of pascal triangles"""
+    list_of_lists = []
+
+    for i in range(n):
+        if i == 0:
+            list_of_lists.append([1])
+        elif i == 1:
+            list_of_lists.append([1, 1])
+        else:
+            last_list = list_of_lists[-1]
+            count = 0
+            new_list = [1]
+            while count < len(last_list):
+                try:
+                    new_list.append(last_list[count] + last_list[count + 1])
+                    count += 1
+                except IndexError:
+                    break
+            new_list.append(1)
+            list_of_lists.append(new_list)
+    return list_of_lists
