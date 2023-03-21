a4f97d810b654b3f8eb0c65912013e83abfe64a5
Malongmak <j.malong@alustudent.com>
2023-03-21 22:29:48 +0200

    3-to_json_string.py

---
+++ python-input_output/3-main.py
@@ -0,0 +1,6 @@
+3-main.py
+[1, 2, 3]
+<class 'str'>
+{"id": 12, "is_active": true, "name": "John", "info": {"average": 3.14, "age": 36}, "places": ["San Francisco", "Tokyo"]}
+<class 'str'>
+[TypeError] {3, 132} is not JSON serializable
