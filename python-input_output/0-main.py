99ac2c64c3193a0e79b42576e90402caa0185024
Malongmak <j.malong@alustudent.com>
2023-03-21 22:17:50 +0200

    0-read_file.py

---
+++ python-input_output/0-main.py
@@ -0,0 +1,4 @@
+0-main.py
+We offer a truly innovative approach to education:
+focus on building reliable applications and scalable systems, take on real-world challenges, collaborate with your peers. 
+
