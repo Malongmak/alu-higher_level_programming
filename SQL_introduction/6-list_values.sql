9bccfdefeda376a3afc20714e025b51e323a561d
malong_joshua <121355936+Malongmak@users.noreply.github.com>
2023-03-25 01:02:12 +0200

    Update 6-list_values.sql

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,9 +1,3 @@
--- script that creates the database hbtn_0d_usa and the 
--- table states (in the database hbtn_0d_usa) on your MySQL server.
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (
-    id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    name VARCHAR(256)
-);
-
+-- Lists all rows of a table
+-- Lists all rows of the table first_table from the database hbtn_0c_0
+SELECT * FROM first_table;
