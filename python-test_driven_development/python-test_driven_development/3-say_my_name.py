6c57a7973236b8471bd920455cf28a91f4dca0df
Malongmak <j.malong@alustudent.com>
2023-04-08 02:00:45 +0200

    3-say_my_name.py

--- python-test_driven_development/python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/python-test_driven_development/3-say_my_name.py
@@ -16,4 +16,3 @@ def say_my_name(first_name, last_name=""):
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
-
