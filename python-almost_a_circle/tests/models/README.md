fa8916a24adab3b975139c355651890fc79794b5
Malongmak <j.malong@alustudent.com>
2023-04-27 09:53:51 +0200

    tests

--- python-almost_a_circle/tests/models/README.md
+++
@@ -1,1 +0,0 @@
-python-almost a circle
--- python-almost_a_circle/tests/tests_models/test_square.py
+++ python-almost_a_circle/tests/tests_models/test_square.py
@@ -22,76 +22,6 @@ class TestSquare(unittest.TestCase):
         s0 = Square(1, 2, 3, 4)
 
         self.assertEqual(s0.id, 4)
-with self.assertRaisesRegex(ValueError, "width must be > 0"):
-            s9 = Square(-1, 2)
-
-        with self.assertRaisesRegex(ValueError, "x must be >= 0"):
-            s10 = Square(1, -2)
-
-        with self.assertRaisesRegex(ValueError, "width must be > 0"):
-            s11 = Square(0, 2)
-
-        with self.assertRaisesRegex(ValueError, "y must be >= 0"):
-            s13 = Square(1, 2, -3)
-
-        with self.assertRaisesRegex(ValueError, "width must be > 0"):
-            s = Square(0)
-
-        with self.assertRaisesRegex(TypeError, "width must be an integer"):
-            s4 = Square("1")
-
-        with self.assertRaisesRegex(TypeError, "x must be an integer"):
-            s5 = Square(1, "2")
-
-        with self.assertRaisesRegex(TypeError, "y must be an integer"):
- s6 = Square(1, 2, "3")
-
-    def test_area(self):
-        """Test the functions in square"""
-        s1 = Square(2)
-        self.assertEqual(s1.area(), 4)
-
-    def test_str_(self):
-        """Test the functions in square"""
-        Base.Base_nb_objects = 0
-        s1 = Square(2)
-        with patch("sys.stdout", new=StringIO()) as fake_out:
-            print(s1)
-            self.assertEqual(fake_out.getvalue(),
-                             "[Square] (1) 0/0 - 2\n")
-
-    def test_display(self):
-        """Test the functions in square"""
-        s1 = Square(2)
-        s2 = Square(2, 2, 3)
-        with patch("sys.stdout", new=StringIO()) as fake_out:
-            s1.display()
-and add the following code:
-
-#!/usr/bin/python3
-"""Test for Square"""
-
-import os
-import unittest
-from io import StringIO
-from unittest.mock import patch
-
-from models.base import Base
-from models.square import Square
-
-
-class TestSquare(unittest.TestCase):
-    """Test for class Square"""
-
-    def test_instance(self):
-        """Test the functions in square"""
-        s = Square(1)
-        s1 = Square(1, 2)
-        s2 = Square(1, 2, 3)
-        s12 = Square(1, 0)
-        s0 = Square(1, 2, 3, 4)
-
-        self.assertEqual(s0.id, 4)
 
         with self.assertRaisesRegex(ValueError, "width must be > 0"):
             s9 = Square(-1, 2)
@@ -150,7 +80,8 @@ class TestSquare(unittest.TestCase):
         s1 = Square(4)
         self.assertEqual(s1.to_dictionary(),
                          {'id': 1, 'size': 4, 'x': 0, 'y': 0})
-def test_update(self):
+
+    def test_update(self):
         """Test the functions in square"""
         Base.Base_nb_objects = 0
         s1 = Square(2)
@@ -168,11 +99,14 @@ def test_update(self):
         self.assertEqual(s1.id, 89)
         self.assertEqual(s1.size, 1)
         self.assertEqual(s1.x, 2)
- s1.update(89, 1, 2, 3)
+
+        s1.update(89, 1, 2, 3)
         self.assertEqual(s1.id, 89)
         self.assertEqual(s1.size, 1)
         self.assertEqual(s1.x, 2)
-        self.assertEqual(s1.y, 3)  s1.update(**{'id': 89})
+        self.assertEqual(s1.y, 3)
+
+        s1.update(**{'id': 89})
         self.assertEqual(s1.id, 89)
 
         s1.update(**{'id': 89, 'size': 1})
@@ -183,7 +117,8 @@ def test_update(self):
         self.assertEqual(s1.id, 89)
         self.assertEqual(s1.size, 1)
         self.assertEqual(s1.x, 2)
- s1.update(**{'id': 89, 'size': 1, 'x': 2, 'y': 3})
+
+        s1.update(**{'id': 89, 'size': 1, 'x': 2, 'y': 3})
         self.assertEqual(s1.id, 89)
         self.assertEqual(s1.size, 1)
         self.assertEqual(s1.x, 2)
@@ -198,7 +133,8 @@ def test_update(self):
         s1 = Square.create(**{'id': 89, 'size': 1})
         self.assertEqual(s1.id, 89)
         self.assertEqual(s1.size, 1)
- s1 = Square.create(**{'id': 89, 'size': 1, 'x': 2})
+
+        s1 = Square.create(**{'id': 89, 'size': 1, 'x': 2})
         self.assertEqual(s1.id, 89)
         self.assertEqual(s1.size, 1)
         self.assertEqual(s1.x, 2)
@@ -216,7 +152,8 @@ def test_update(self):
         self.assertEqual(s1.size, 1)
         self.assertEqual(s1.x, 2)
         self.assertEqual(s1.y, 3)
-  def test_save_to_file(self):
+
+    def test_save_to_file(self):
         """Test the functions in square"""
         Base.Base_nb_objects = 0
 
@@ -237,7 +174,7 @@ def test_update(self):
 
     def test_save_to_file_empty(self):
         Square.save_to_file([])
- self.assertTrue(os.path.isfile("Square.json"))
+        self.assertTrue(os.path.isfile("Square.json"))
         with open("Square.json") as file:
             self.assertEqual(file.read(), "[]")
             self.assertEqual(type(file.read()), str)
