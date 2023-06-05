825eac881be8da3389fcdb2f667064fabcc84177
malong_joshua <121355936+Malongmak@users.noreply.github.com>
2023-06-05 23:13:34 +0300

    Update 5-to_integer.js

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -3,5 +3,5 @@ const number = parseInt(process.argv[2]);
 if (Number.isNaN(number)) {
   console.log('Not a number');
 } else {
-  console.log('My number:' + number);
+  console.log('My number: ' + number);
 }
