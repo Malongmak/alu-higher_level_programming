4cc958488bcbd515a8078ebce6910beaa251c3b0
Malongmak <j.malong@alustudent.com>
2023-06-13 09:27:08 +0300

    101-script.js

---
+++ javascript-web_jquery/101-script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $('#add_item').click(function() {
+    $('ul.my_list').append('<li>Item</li>');
+  });
+
+  $('#remove_item').click(function() {
+    $('ul.my_list li:last').remove();
+  });
+
+  $('#clear_list').click(function() {
+    $('ul.my_list').empty();
+  });
+});
+
