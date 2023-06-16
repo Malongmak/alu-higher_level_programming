d0440fb949e3bcbfd1d6f1eb0ff179d4068014c1
Malongmak <j.malong@alustudent.com>
2023-06-16 10:55:11 +0300

    101-starwars_characters.js

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -1,54 +1,34 @@
 #!/usr/bin/node
+
 const request = require('request');
 
-function printCharacters(movieId) {
-  const baseUrl = 'https://swapi.dev/api/films/';
-  const movieUrl = `${baseUrl}${movieId}/`;
-
-  request.get(movieUrl, (error, response, body) => {
-    if (error) {
-      console.error('An error occurred:', error);
-      return;
-    }
-
-    if (response.statusCode !== 200) {
-      console.error('Invalid request:', response.statusCode);
-      return;
-    }
-
-    const movieData = JSON.parse(body);
-    console.log(`Characters in ${movieData.title}:`);
-
-    const characterPromises = movieData.characters.map((characterUrl) => {
-      return new Promise((resolve, reject) => {
-        request.get(characterUrl, (error, response, body) => {
-          if (error) {
-            reject(error);
-            return;
-          }
-
-          if (response.statusCode !== 200) {
-            reject(`Invalid request: ${response.statusCode}`);
-            return;
-          }
-
-          const characterData = JSON.parse(body);
-          resolve(characterData.name);
-        });
-      });
-    });
-
-    Promise.all(characterPromises)
-      .then((characterNames) => {
-        characterNames.forEach((characterName) => {
-          console.log(characterName);
-        });
-      })
-      .catch((error) => {
-        console.error('An error occurred:', error);
-      });
-  });
-}
-
 const movieId = process.argv[2];
-printCharacters(movieId);
+const url = `https://swapi.dev/api/films/${movieId}/`;
+let characters = [];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  const data = JSON.parse(body);
+  characters = data.characters;
+  getCharacters(0);
+});
+
+const getCharacters = (index) => {
+  if (index === characters.length) {
+    return;
+  }
+
+  request(characters[index], (error, response, body) => {
+    if (error) {
+      console.log(error);
+      return;
+    }
+    const characterData = JSON.parse(body);
+    console.log(characterData.name);
+    getCharacters(index + 1);
+  });
+};
